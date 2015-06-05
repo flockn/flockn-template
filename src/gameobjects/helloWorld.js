b57a9c7a17f6f0db624ea25b27b6ffe6f8f2a2ad
Johannes Stein <johannesstein@freeze-dev.com>
2015-06-05 18:51:16 +0200

    Seperata gameobject

---
+++ src/gameobjects/helloWorld.js
@@ -0,0 +1,10 @@
+export default function() {
+    // The logic for this game object
+
+    // Set the position for this game object
+    this.position.x = 100;
+    this.position.y = 100;
+
+    // This game object now holds a label with the text "Hello World"
+    this.texture.label.text = 'Hello World';
+}
